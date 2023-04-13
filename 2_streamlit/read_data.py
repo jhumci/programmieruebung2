ebaa153e0870eb88b2bb8d84a849bf6f80ad9333
jhumci <julian.huber@mci.edu>
2023-04-13 11:25:46 +0200

    added streamlit example

---
+++ 2_streamlit/read_data.py
@@ -0,0 +1,49 @@
+#%%
+import json
+
+# Opening JSON file
+def load_person_data():
+    file = open("../data/person_db.json")
+    person_data = json.load(file)
+    return person_data
+# %%
+
+def get_person_list(person_data):
+    list_of_names = []
+
+    for eintrag in person_data:
+        list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
+    return list_of_names
+
+
+
+# %% Test
+#get_person_list(load_person_data())
+
+
+# %%
+
+def find_person_data_by_name(suchstring):
+    person_data = load_person_data()
+    #print(suchstring)
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        print(eintrag)
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            print()
+
+            return eintrag
+    else:
+        return {}
+
+# %% Test
+#current_person = find_person_data_by_name("Statham, Jason")
+#current_person
+#current_picture_path = current_person["picture_path"]
+#current_picture_path
